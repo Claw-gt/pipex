b8ef41f2fff47eeded3667c8c77c63dfd63d75ac
Claudia <claudiagatalavera@gmail.com>
2024-09-25 12:31:45 +0200

    enumerator deleted

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: clagarci <clagarci@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/09/01 11:06:50 by clagarci          #+#    #+#              #
-#    Updated: 2024/09/24 14:01:49 by clagarci         ###   ########.fr        #
+#    Updated: 2024/09/25 11:51:14 by clagarci         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -14,7 +14,7 @@ NAME = pipex
 
 
 LIBDIR = libft/
-MY_SOURCES = src/pipex.c src/trying.c
+MY_SOURCES = src/pipex.c #src/trying.c
 
 MY_OBJECTS = $(MY_SOURCES:.c=.o)
 
--- inc/pipex.h
+++ inc/pipex.h
@@ -6,7 +6,7 @@
 /*   By: clagarci <clagarci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/23 17:23:15 by clagarci          #+#    #+#             */
-/*   Updated: 2024/09/23 19:15:50 by clagarci         ###   ########.fr       */
+/*   Updated: 2024/09/25 12:08:00 by clagarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,22 +16,12 @@
 # include <string.h>
 # include <unistd.h>
 # include <sys/wait.h>
+# include <errno.h>
+# include <fcntl.h>
 # include "../libft/libft.h"
 
 enum
 {
-	ERROR_BLOCK_OPERATION = 1,
-	ERROR_FILE = 2,
-	ERROR_UNKNOWN_PROCESS = 3,
-	ERROR_INTERRUPT_CALL = 4,
-	ERROR_IO = 5,
-	ERROR_ADDRESS = 6,
-	ERROR_LONG_ARGS = 7,
-	ERROR_EXEC_FORMAT = 8,
-	ERROR_FD = 9,
-	ERROR_CHILD_PROC = 10,
-    ERROR_RESOURCE_UNAVAILABLE = 11,
-    ERROR_MEMORY_ALLOC = 12,
-    ERROR_NO_PERMISSION = 13,
+	ERROR_ARGS_NUMBER = 1,
 };
 #endif
