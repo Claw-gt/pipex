e0f30854a71017706b76d3abbcad5de516936e1d
Claudia <claudiagatalavera@gmail.com>
2024-10-03 14:11:44 +0200

    solved norminette

--- src/errors.c
+++ src/errors.c
@@ -6,20 +6,20 @@
 /*   By: clagarci <clagarci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/29 17:39:54 by clagarci          #+#    #+#             */
-/*   Updated: 2024/09/30 18:51:27 by clagarci         ###   ########.fr       */
+/*   Updated: 2024/10/03 14:11:31 by clagarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/pipex.h"
 
-void    print_errno(char *str)
+void	print_errno(char *str)
 {
-    perror(str);
+	perror(str);
 	exit (EXIT_FAILURE);
 }
 
-void    custom_error(char *str)
+void	custom_error(char *str)
 {
-    ft_putstr_fd(str, 2);
-    exit (EXIT_FAILURE);
+	ft_putstr_fd(str, 2);
+	exit (EXIT_FAILURE);
 }
